ee95ded23260829e3301a20adbfcb1842cfa4807
Sohaib Nehal <sohaib.nehal@ymail.com>
2018-04-09 11:14:28 +0500

    Adding routing to the angular app

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from "./service/in-memory-data.service";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import {HeroService} from "./service/hero.service";
 import { MessageService } from './service/message.service';
-import { MessageComponent } from './message/message.component';
+
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +30,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [
     HeroService,
--- angular-app/src/app/service/hero.service.ts
+++ angular-app/src/app/service/hero.service.ts
@@ -1,27 +1,67 @@
 import { Injectable } from '@angular/core';
 import { Hero } from "../interface/hero";
-import { HEROES } from "../data/heroes";
 import { MessageService } from './message.service';
+
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 
 import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.addMessage('Hero Service: Heroes have been fetched');
-    return of (HEROES);
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.addMessage('HeroService: ' + message);
+  }
+
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        // Tap operator, looks at the Observable values, does something wit values, and pass them along.
+        tap(heroes => this.log(`Heroes fetched`)),
+        // catchError operator intercepts the Observable that failed.
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.addMessage(`Hero Service: Sending hero with id: ${id}`);
-    return of (HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap (hero => this.log(`Hero fetched`)),
+        catchError(this.handleError('gerHero', []))
+      );
+  }
 
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 
